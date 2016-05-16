692841ee87c3cdf6c4ec14eb60677880c601b68a
CryptAxe <cryptaxe@gmail.com>
2016-05-16 14:57:15 -0700

    Update MarketTradeView

--- src/qt/markettradeview.cpp
+++ src/qt/markettradeview.cpp
@@ -44,7 +44,7 @@ void MarketTradeView::on_pushButtonFinalize_clicked()
     std::string marketHex = ui->labelMarketIDValue->text().toStdString();
     std::string type = (ui->radioButtonLong->isChecked())? "buy":"sell";
     double shares = ui->labelSharesToBuyValue->text().toDouble();
-    double price = ui->labelCurrentPriceValue->text().toDouble();
+    double price = ui->doubleSpinBoxOrderPrice->value();
     int state = ui->spinBoxState->value();
     int nonce = 0;
 
@@ -122,9 +122,10 @@ void MarketTradeView::updateMarketInfo()
     // Shares on the market
     double *nShares = new double [nStates];
     marketNShares(trades, nStates, nShares);
-    std::string marketPrice = FormatMoney(marketAccountValue(market->maxCommission, 1e-8*market->B, nStates, nShares));
 
-    // Set market price label
+    // Price of the market
+    double accountValue = marketAccountValue(market->maxCommission, 1e-8*market->B, nStates, nShares);
+    std::string marketPrice = FormatMoney(accountValue);
     ui->labelCurrentPriceValue->setText(QString::fromStdString(marketPrice));
 }
 
